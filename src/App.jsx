c567186b3c3739b6417788a94e9cd72976d78286
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2022-07-05 00:09:50 +0430

    Navbar component Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+`;
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
 };
 
 export default App;
