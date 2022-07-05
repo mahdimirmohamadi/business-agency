59e2692ab07ab561bb3ec965f7d704bddc5717b2
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2022-07-05 16:48:16 +0430

    Into componenet & AnimatedShapes component handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
 
 const Container = styled.div`
   height: 100vh;
+  overflow: hidden;
 `;
 
-const Wrapper = styled.div`
-  padding: 20px 10px;
+const IntroShape = styled.div`
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  z-index: -1;
 `;
+
 const App = () => {
   return (
     <Container>
       <Navbar />
+      <Intro />
+      <IntroShape />
     </Container>
   );
 };
